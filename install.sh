8bb499e87ecda9970f83ae34f56f43d437972ed4
Autumn <149750837+aut-mn@users.noreply.github.com>
2024-05-16 09:46:05 +0000

    Better installer

--- install.sh
+++ install.sh
@@ -3,12 +3,16 @@
 # Installs EVERYTHING!
 # .bashrc could cause problems in non-fedora distros
 
-if [ -f "$HOME/.bashrc" ] || [ -f "$HOME/.vimrc" ]; then
-    read -p "Files .vimrc and .bashrc already exist. Do you want to overwrite them? [Y/n] " choice
+if [ -f ".bashrc" ] || [ -f ".vimrc" ]; then
+    read -p "Install vimrc & bashrc profiles? [Y/n] " choice
     case "$choice" in
         [Yy]* )
-            mv "$HOME/.vimrc" "$HOME/.vimrc.bak" && echo "Backed up old .vimrc"
-            mv "$HOME/.bashrc" "$HOME/.bashrc.bak" && echo "Backed up old .bashrc"
+            if [ -f "$HOME/.vimrc" ]; then
+                mv "$HOME/.vimrc" "$HOME/.vimrc.bak" && echo "Backed up old .vimrc"
+            fi
+            if [ -f "$HOME/.bashrc" ]; then
+                mv "$HOME/.bashrc" "$HOME/.bashrc.bak" && echo "Backed up old .bashrc"
+            fi
             mv ".vimrc" "$HOME" && echo "New .vimrc installed"
             mv ".bashrc" "$HOME" && echo "New .bashrc installed"
             echo "Done"
@@ -17,9 +21,9 @@ if [ -f "$HOME/.bashrc" ] || [ -f "$HOME/.vimrc" ]; then
             echo "Aborted"
             ;;
         * )
-            echo "Invalid choice, aborted."
+            echo "Aborted"
             ;;
     esac
 else
-    echo "Files not found. Nothing to install."
+    echo "Files not found, nothing to install"
 fi
